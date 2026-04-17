ae14dc0afd35064ccc5703b0e7c69a765480ece6
Steele Shreve <steele@steelesh.dev>
2026-04-17 16:52:18 -0500

    chore: regenerate

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,10 +1,12 @@
 return {
   "sindrets/diffview.nvim",
-  cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-  keys = {
-    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diff view" },
-    { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File history" },
-    { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Branch history" },
-  },
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
   opts = {},
+  keys = {
+    { "<leader>gdo", "<cmd>DiffviewOpen<cr>",                    desc = "Diffview: working tree" },
+    { "<leader>gdh", "<cmd>DiffviewFileHistory % --follow<cr>",  desc = "Diffview: file history (rename-aware)" },
+    { "<leader>gdH", "<cmd>DiffviewFileHistory<cr>",             desc = "Diffview: branch history" },
+    { "<leader>gdc", "<cmd>DiffviewClose<cr>",                   desc = "Diffview: close" },
+  },
 }
--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -37,7 +37,31 @@ return {
       map("v", "<leader>ghs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, { desc = "Stage hunk" })
       map("v", "<leader>ghr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, { desc = "Reset hunk" })
       map("n", "<leader>ghp", gs.preview_hunk, { desc = "Preview hunk" })
-      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, { desc = "Blame line" })
+      map("n", "<leader>ghb", function()
+        gs.blame_line({ full = true, ignore_whitespace = true, extra_opts = { "-M", "-C" } })
+      end, { desc = "Blame line (with move/copy detection)" })
+      map("n", "<leader>gB", gs.blame, { desc = "Blame file (split)" })
+      map("v", "<leader>ghB", function()
+        local s, e = vim.fn.line("."), vim.fn.line("v")
+        if s > e then s, e = e, s end
+        local file = vim.fn.expand("%:p")
+        local out = vim.fn.systemlist(("git -C %s blame -L %d,%d -M -C --porcelain -- %s"):format(
+          vim.fn.shellescape(vim.fn.fnamemodify(file, ":h")), s, e, vim.fn.shellescape(file)))
+        local authors = {}
+        for _, l in ipairs(out) do
+          local a = l:match("^author (.+)")
+          if a and a ~= "Not Committed Yet" then
+            authors[a] = (authors[a] or 0) + 1
+          end
+        end
+        local list = {}
+        for a, n in pairs(authors) do table.insert(list, ("%s (%d)"):format(a, n)) end
+        if #list == 0 then
+          vim.notify("No commit info for range", vim.log.levels.WARN, { title = ("Range %d-%d"):format(s, e) })
+        else
+          vim.notify(table.concat(list, ", "), vim.log.levels.INFO, { title = ("Authors %d-%d"):format(s, e) })
+        end
+      end, { desc = "Range authors (unique)" })
       map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle line blame" })
     end,
   },
